0e8406d137a3f20f8c8ce325da6b7bebc5375b41
Ipvikukiepki <32176631+Naras-KS@users.noreply.github.com>
2020-10-26 16:50:44 +0100

    Update ds_format.py

--- Datasets/ds_format.py
+++ Datasets/ds_format.py
@@ -8,7 +8,7 @@ def cli1():
 @cli1.command()
 
 #In default 'Relative Path' of the Testsuit and Testcase has provided(from os module '../' represents one level up to the current Working directory)
-@click.option('--infile', default = 'train/train_1_01.json', help = 'Loading JSON file in the specified path')
+@click.option('--infile', default = 'SGD dataset/train/train_1_01.json', help = 'Loading JSON file in the specified path')
 @click.option('--outnlu', default = 'NLU_traindata.json', help = 'output customized JSON file in the specified path')
 @click.option('--outdm', default = 'DM_stories.md', help = 'output customized conversation as md file in the specified path')
 @click.option('--outnlg', default = 'NLG_traindata.json', help = 'output customized NLG as JSON file in the specified path')
