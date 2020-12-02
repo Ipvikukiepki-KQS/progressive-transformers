8a5ec727ae33d8601a110159aa45315c119550de
Ipvikukiepki <32176631+Naras-KS@users.noreply.github.com>
2020-12-02 21:16:06 +0100

    Dataset Customization Update

--- Datasets/customize_data.py
+++ Datasets/customize_data.py
@@ -14,55 +14,83 @@ class DataCustomization(object):
             return sgd_data
     
     @staticmethod
-    def trainData(data_path, out_nlu):
+    def trainData(data_path, out_nlu, C):
         print(data_path)
-        keys = []
         data_access = DataCustomization.dataRead(data_path)        
-
-        def checkData(data):
+        def checkData(data, C):
             if isinstance(data, dict):
-                data = checkDictData(data)
-            elif isinstance(data, list):
-                data = getListData(data)
+                data = getDictData(data, C)
             elif isinstance(data, str):
                 data = printStrData(data)
             return data
 
-        def checkDictData(dict_data):
+        def checkDictData(dict_data, C, c_R, c_H, c_A, c_T, c_P, c_HO):
             
             for items in dict_data:
                 if "goal" in items:
                     nest_items = dict_data[items]
                     for elements in nest_items:
                         if "restaurant" in elements:
-                            check_rest = len(nest_items[elements])
+                            c_R = len(nest_items[elements])
 
                         if "hotel" in elements:
-                            check_hotel = len(nest_items[elements])
+                            c_H = len(nest_items[elements])
                         
                         if "attraction" in elements:
-                            check_attract = len(nest_items[elements])
+                            c_A = len(nest_items[elements])
                         
                         if "taxi" in elements:
-                            check_taxi = len(nest_items[elements])
+                            c_T = len(nest_items[elements])
                         
                         if "police" in elements:
-                            check_police = len(nest_items[elements])
+                            c_P = len(nest_items[elements])
                         
                         if "hospital" in elements:
-                            check_hospital = len(nest_items[elements])
-
+                            c_HO = len(nest_items[elements])
                 if "log" in items:
-                    checkData(dict_data[items], count = 1)
+                    dict_data = dict_data[items]
+            return c_R, c_H, c_A, c_T, c_P, c_HO, dict_data
 
-            return check_rest, check_hotel, check_attract, check_taxi, check_police, check_hospital
+        def getDictData(dict_data, C):
+            if C == 0:
+                c_R, c_H, c_A, c_T, c_P, c_HO, dict_data = checkDictData(dict_data, C, c_R=0, c_H=0, c_A=0, c_T=0, c_P=0, c_HO=0)
+                C = 1
+            if C == 1 :
+                if dict_data is not None and c_R != 0 and  c_H == 0 and c_A == 0 and  c_T == 0 and c_P == 0 and c_HO == 0:
+                        #dict_data = getListData(dict_data, C)
+                    if isinstance(dict_data,list):
+                        dict_data = getListData(dict_data, C)
+                    elif isinstance(dict_data,dict):
+                        dict_data = dict_data
+                    for items in dict_data:
+                        if "text" in items:
+                            dialogue = dict_data[items]
+                            print("the text is", dialogue)
+                        
+                        if "intent" in items:
+                            nest_intent = list(dict_data[items].keys())
+                            print("the intent is", nest_intent)
+                            print("intent entities are", dict_data[items])
+                            int_ent = dict_data[items]
+                            if isinstance(int_ent,dict):
+                                for nest_entities in int_ent:
+                                    ent_val = int_ent[nest_entities]
+                                    print("entities are", ent_val)
+                                    if isinstance(ent_val, list):
+                                        for ent_pair in ent_val:
+                                            print("the entities and values are", ent_pair)
+                                            count = 1
+                                            for entity in ent_pair:
+                                                if count == 1:
+                                                    print("the entity is", entity)
+                                                if count == 2:
+                                                    print("the value is", entity)
+                                                count += 1       
 
-        def getDictData(dict_data, count = 1):
-            checkData(data, count)
+        def getListData(list_data, C):
 
-        def getListData(list_data):
             for items in list_data:
-                checkData(items)
+                return items
 
         def printStrData(str_data):
             pass
@@ -71,7 +99,7 @@ class DataCustomization(object):
         if isinstance(data_access,dict):
             print("json as a dict")
             for data in data_access:
-                data = checkData(data_access[data], count=0)                        
+                data = checkData(data_access[data], C)                        
         else:
             print("recheck")
 
